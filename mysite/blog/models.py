a9e557adf5014430410334d550063539d7ae58f3
Jase M. Wilson <jase.wilson@aggiemail.usu.edu>
2020-02-26 12:11:11 -0700

    Added prevention to upload db to .gitignore

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=200)
+	author = models.CharField(max_length=200)
+	content = models.TextField()
+	posted = models.DateTimeField()
+	
+	def __str__(self):
+		return self.title
+
+class Comment(models.Model):
+	blog = models.ForeignKey(Blog, on_delete=models.CASCADE,)
+	commenter = models.CharField(max_length=200)
+	email = models.EmailField()
+	content = models.TextField()
+	posted = models.DateTimeField()	
+	
+	def __str__(self):
+		return self.content
